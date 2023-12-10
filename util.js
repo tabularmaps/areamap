f9baba3750c0aa5aa37984577f300add4f54eb4c
Taisuke Fukuno <fukuno@jig.jp>
2023-12-10 11:49:44 +0900

    use ES modules

---
+++ util.js
@@ -0,0 +1,32 @@
+export const create = (tag) => document.createElement(tag);
+export const get = (id) => document.getElementById(id);
+
+export const makeGrids = (stab) => {
+  const area = [];
+  let maxc = 0;
+  const lines = stab.split("\n");
+	for (let a of lines) {
+    const row = a.split(" ");
+    if (row.length > 1 || row[0].length > 0) {
+      area.push(row);
+      if (row.length > maxc) {
+        maxc = row.length;
+      }
+    }
+  }
+  const c = document.createElement("div");
+  c.style.display = "grid";
+  c.style.gridTemplateColumns = `repeat(${maxc}, 1fr)`;
+  c.style.gridTemplateRows = `repeat(${lines.length}, 1fr)`;
+  const areas = [];
+  for (const ar of area) {
+    for (const a of ar) {
+      const div = document.createElement("span");
+      c.appendChild(div);
+      div.textContent = a;
+      areas.push(a);
+    }
+  }
+  c.areas = areas;
+  return c;
+};
